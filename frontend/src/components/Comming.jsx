3fdda146bb342a5b13c0b2699544a7dc3136396e
Ayush Bansal <116891199+Bansalayush247@users.noreply.github.com>
2023-10-28 01:17:46 +0530

    Update Comming.jsx

--- frontend/src/components/Comming.jsx
+++ frontend/src/components/Comming.jsx
@@ -6,6 +6,11 @@ function Comming() {
   return (
     <>
         <div className="comming_soon_container">
+          <div className='coming_soon_text'>
+            <p className='text1'>COMING SOON</p>
+            <p className='text2'>COMING SOON</p>
+            <p className='text3'>COMING SOON</p>
+          </div>
             <img src={des} className='des' alt="" srcset="" />
             <img src={robot}  className="robot"alt="" srcset="" />
         </div>
